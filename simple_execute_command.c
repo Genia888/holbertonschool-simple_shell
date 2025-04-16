90b148a1d1bec09a64595421a545320a4a3a607a
Genia888 <khalepo@gmail.com>
2025-04-16 15:13:23 +0200

    fix int int simple shell file

--- simple_execute_command.c
+++ simple_execute_command.c
@@ -6,35 +6,42 @@
 #include <sys/wait.h>
 
 /**
- * simple_execute_command - Execute command
- * @args: tableau d'arguments
+ * simple_execute_command - Execute une commande
+ * @args: Tableau d'arguments
+ *
+ * Return: Code de retour du programme exécuté, ou 2 si erreur execve
  */
-void simple_execute_command(char **args)
+int simple_execute_command(char **args)
 {
-	char *full_cmd = NULL;
 	pid_t pid;
 	int status;
 
 	if (!args || !args[0])
-		exit(0);
-
-	full_cmd = strdup(args[0]);
-	if (!full_cmd)
-	{
-		dprintf(STDERR_FILENO, "./hsh: 1: %s: not found\n", args[0]);
-		exit(127);
-	}
+		return (0);
 
 	pid = fork();
-	if (pid == 0)
+	if (pid == -1)
 	{
-		execve(full_cmd, args, environ);
-		perror("execve");
-		exit(1);
+		perror("fork");
+		return (2);
+	}
+	else if (pid == 0)
+	{
+		
+		if (execvp(args[0], args) == -1)
+		{
+			perror(args[0]);
+			exit(2);
+		}
 	}
 	else
 	{
 		waitpid(pid, &status, 0);
-		free(full_cmd);
+		if (WIFEXITED(status))
+			return (WEXITSTATUS(status));
+		else
+			return (2);
 	}
+	return (0);
 }
+

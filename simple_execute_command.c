a6620533594d6569f1010f65b32515202e92d83f
SebSa12000 <sebastien.salgues@gmail.com>
2025-04-16 19:08:25 +0200

    add comments

--- simple_execute_command.c
+++ simple_execute_command.c
@@ -14,27 +14,34 @@ void simple_execute_command(char **args)
 	char *full_cmd = NULL;
 	pid_t pid;
 	int status;
-
+	/* if args == NULL or args[0] == NULL => exit(0) */
 	if (!args || !args[0])
 		exit(0);
-
+	/* duplicate the args[0] */
 	full_cmd = strdup(args[0]);
+	/* if we cannot allocate memory display an error message */
 	if (!full_cmd)
 	{
 		dprintf(STDERR_FILENO, "./hsh: 1: %s: not found\n", args[0]);
 		exit(127);
 	}
-
+	/* fork the process */
 	pid = fork();
 	if (pid == 0)
 	{
+		/* system to to the program content in full_cmd variable */
 		execve(full_cmd, args, environ);
+		/* display the error message if there are one */
+		/* normaly the function will be exit before that */
 		perror("execve");
+		/* end of the parent process */
 		exit(1);
 	}
 	else
 	{
+		/* wait the parent process */
 		waitpid(pid, &status, 0);
+		/* free the strdup allocation */
 		free(full_cmd);
 	}
 }

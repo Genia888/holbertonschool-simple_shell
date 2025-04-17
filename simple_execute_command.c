3a134d7f245a33cba2987bee473f78a2c752eb9c
SebSa12000 <sebastien.salgues@gmail.com>
2025-04-17 18:51:25 +0200

    betty

--- simple_execute_command.c
+++ simple_execute_command.c
@@ -8,64 +8,46 @@
 /**
  * simple_execute_command - Execute command
  * @args: tableau d'arguments
+ * Return: exit value
  */
 int simple_execute_command(char **args)
 {
 	char *full_cmd = NULL;
 	pid_t pid;
-	int status;
-	int exist_cmd = 0;
-	/* if args == NULL or args[0] == NULL => exit(0) */
-	if (!args || !args[0])
+	int status, exist_cmd = 0;
+
+	if (!args || !args[0]) /* if args == NULL or args[0] == NULL => exit(0) */
 		return (0);
 	/* duplicate the args[0] or search path */
 	if (strchr(args[0], '/'))
-	{
 		full_cmd = strdup(args[0]);
-		exist_cmd = exist_command(full_cmd);
-	}
 	else
-	{
 		full_cmd = search_path(args[0]);
-		exist_cmd = exist_command(full_cmd);
-	}
+	exist_cmd = exist_command(full_cmd);
 	/* if we cannot allocate memory display an error message */
-	if (!full_cmd)
+	if (!full_cmd || !exist_cmd)
 	{
 		fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
 		return (127);
 	}
-	/* if we cannot allocate memory display an error message */
-	if (!exist_cmd)
-	{
-		fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
-		return (127);
-	}
-	/* fork the process */
-	pid = fork();
+	pid = fork(); /* fork the process */
 	if (pid == -1)
 	{
 		perror("fork");
 		free(full_cmd);
 		return (2);
 	}
-
 	if (pid == 0) /* Processus enfant */
 	{
 		execve(full_cmd, args, environ);
-		/* Si execve échoue, afficher une erreur et quitter */
 		perror("execve");
 		free(full_cmd);
 		return (2);
-	}
-
-	/* Parent : attendre la fin du processus enfant */
+	} /* Parent : attendre la fin du processus enfant */
 	waitpid(pid, &status, 0);
-	free(full_cmd);
-
-	/* Retourner le code de sortie du processus enfant */
+	free(full_cmd); /* Retourner le code de sortie du processus enfant */
 	if (WIFEXITED(status))
 		return (WEXITSTATUS(status));
 	else
-		return (2);  /* Erreur générique si le processus n’a pas quitté normalement */
+		return (2);
 }

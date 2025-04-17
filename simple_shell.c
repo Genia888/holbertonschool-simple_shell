9a0ee6eafc2bbd6e2beca0a16c7af0849b623dd7
SebSa12000 <sebastien.salgues@gmail.com>
2025-04-17 19:01:33 +0200

    betty

--- simple_shell.c
+++ simple_shell.c
@@ -3,12 +3,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-
 /**
  * main - Shell minimal
  * Return: Always 0
  */
-
 int main(void)
 {
 	int interactive_mode = isatty(STDIN_FILENO) && isatty(STDERR_FILENO);
@@ -33,13 +31,11 @@ int main(void)
 			token = strtok(NULL, " \t\n");
 		}
 		args[i] = NULL;
-
 		if (args[0] && strcmp(args[0], "exit") == 0)
 		{
 			free(line);
 			exit(status);
 		}
-
 		/* execute the commande */
 		if (args[0])
 			status = simple_execute_command(args);
@@ -47,7 +43,6 @@ int main(void)
 		if (interactive_mode)
 			printf("($) ");
 	}
-
 	/* free memory of line */
 	free(line);
 	/* return code to the system */

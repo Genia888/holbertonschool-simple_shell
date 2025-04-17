14371bd570eb0170dd835f830ec486c00cb6fa3e
Genia888 <khalepo@gmail.com>
2025-04-17 15:39:46 +0200

    change parameters of exit in shell and execute

--- simple_execute_command.c
+++ simple_execute_command.c
@@ -9,39 +9,47 @@
  * simple_execute_command - Execute command
  * @args: tableau d'arguments
  */
-void simple_execute_command(char **args)
+int simple_execute_command(char **args)
 {
 	char *full_cmd = NULL;
 	pid_t pid;
 	int status;
 	/* if args == NULL or args[0] == NULL => exit(0) */
 	if (!args || !args[0])
-		exit(0);
+		return (0);
 	/* duplicate the args[0] */
 	full_cmd = strdup(args[0]);
 	/* if we cannot allocate memory display an error message */
 	if (!full_cmd)
 	{
 		dprintf(STDERR_FILENO, "./hsh: 1: %s: not found\n", args[0]);
-		exit(127);
+		return (127);
 	}
 	/* fork the process */
 	pid = fork();
-	if (pid == 0)
+	if (pid == -1)
 	{
-		/* system to to the program content in full_cmd variable */
-		execve(full_cmd, args, environ);
-		/* display the error message if there are one */
-		/* normaly the function will be exit before that */
-		perror("execve");
-		/* end of the parent process */
-		exit(1);
-	}
-	else
-	{
-		/* wait the parent process */
-		waitpid(pid, &status, 0);
-		/* free the strdup allocation */
+		perror("fork");
 		free(full_cmd);
+		return (2);
 	}
+
+	if (pid == 0) /* Processus enfant */
+	{
+		execve(full_cmd, args, environ);
+		/* Si execve échoue, afficher une erreur et quitter */
+		perror("execve");
+		free(full_cmd);
+		exit(2);
+	}
+
+	/* Parent : attendre la fin du processus enfant */
+	waitpid(pid, &status, 0);
+	free(full_cmd);
+
+	/* Retourner le code de sortie du processus enfant */
+	if (WIFEXITED(status))
+		return (WEXITSTATUS(status));
+	else
+		return (2);  /* Erreur générique si le processus n’a pas quitté normalement */
 }

08d9d1c48a8bce1b431502af4cc8794d7b703749
SebSa12000 <sebastien.salgues@gmail.com>
2025-04-18 09:24:21 +0200

    rollback

--- simple_execute_command.c
+++ simple_execute_command.c
@@ -6,18 +6,6 @@
 #include <sys/wait.h>
 
 /**
- * free_str - free a string
- * @flag: flag
- * @str: strig to free
- */
-void free_str(int flag, char *str)
-{
-	if (flag)
-		free(str);
-}
-
-
-/**
  * simple_execute_command - Execute command
  * @args: tableau d'arguments
  * Return: exit value
@@ -26,15 +14,12 @@ int simple_execute_command(char **args)
 {
 	char *full_cmd = NULL;
 	pid_t pid;
-	int status, exist_cmd = 0, flag = 0;
+	int status, exist_cmd = 0;
 
 	if (!args || !args[0]) /* if args == NULL or args[0] == NULL => exit(0) */
 		return (0);
 	if (strchr(args[0], '/'))
-	{
 		full_cmd = strdup(args[0]);
-		flag = 1;
-	}
 	else
 		full_cmd = search_path(args[0]);
 	exist_cmd = exist_command(full_cmd);
@@ -48,18 +33,18 @@ int simple_execute_command(char **args)
 	if (pid == -1)
 	{
 		perror("fork");
-		free_str(flag, full_cmd);
+		free(full_cmd);
 		return (2);
 	}
 	if (pid == 0) /* Processus enfant */
 	{
 		execve(full_cmd, args, environ);
 		perror("execve");
-		free_str(flag, full_cmd);
+		free(full_cmd);
 		return (2);
 	} /* Parent : attendre la fin du processus enfant */
 	waitpid(pid, &status, 0);
-	free_str(flag, full_cmd); /* Retourner le code de sortie du processus enfant */
+	free(full_cmd); /* Retourner le code de sortie du processus enfant */
 	if (WIFEXITED(status))
 		return (WEXITSTATUS(status));
 	else

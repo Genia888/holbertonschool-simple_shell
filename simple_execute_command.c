df3dd2cb89c5ccd4dd5e671ca01cc953ba3cc0e7
SebSa12000 <sebastien.salgues@gmail.com>
2025-04-22 09:45:52 +0200

    betty problem

--- simple_execute_command.c
+++ simple_execute_command.c
@@ -6,22 +6,6 @@
 #include <sys/wait.h>
 
 /**
- * init_args - first test args
- * @args: arguments
- * @full_cmd: long path for the command
- * @exist_cmd: if the command exist
- */
-void init_args(char **args, char **full_cmd, int *exist_cmd)
-{
-	if (strchr(args[0], '/'))
-		*full_cmd = strdup(args[0]);
-	else
-		*full_cmd = search_path(args[0]);
-	if (full_cmd != NULL)
-		*exist_cmd = exist_command(*full_cmd);
-}
-
-/**
  * simple_execute_command - Execute command
  * @args: tableau d'arguments
  * Return: exit value
@@ -34,7 +18,12 @@ int simple_execute_command(char **args)
 
 	if (!args || !args[0]) /* if args == NULL or args[0] == NULL => exit(0) */
 		return (0);
-	init_args(args, &full_cmd, &exist_cmd);
+	if (strchr(args[0], '/'))
+		full_cmd = strdup(args[0]);
+	else
+		full_cmd = search_path(args[0]);
+	if (full_cmd != NULL)
+		exist_cmd = exist_command(full_cmd);
 	/* check if command dont be found or command not exist and not executable */
 	if (full_cmd == NULL || exist_cmd == 0)
 	{
